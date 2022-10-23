ffa9263dfe1a90d63f243c0331859843ec3494fa
zun1uwu <101455048+zun1uwu@users.noreply.github.com>
2022-10-23 19:10:52 +0200

    Changed error handling, fixed mistakes

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,53 @@
-#![windows_subsystem = "windows"]
 #![feature(option_result_contains)]
 
-use std::{env, vec, fs::{self, File}, path::Path, io::Write};
 use scraper::{Html, Selector};
+use std::{
+    env,
+    fs::{self, File},
+    io::Write,
+    path::Path,
+};
 
 fn main() {
-    
+    let cachestring = format!(
+        r"C:\Users\{}\AppData\Roaming\Project Black Pearl\STEAMRIP_Cache.json",
+        whoami::username()
+    );
+    let cachedir = Path::new(&cachestring);
+
+    if cachedir.exists() {
+        println!(r"Clearing previous cache.");
+        fs::remove_file(cachedir).expect("Couldn't delete config file for re-caching.");
+    }
+
     let query: Vec<String> = env::args().collect();
 
     if query.len() < 2 {
-        println!(r#"You must provide a search query, e.g. "php_1337x_scraper 'gta'""#);
+        println!(r#"You must provide a search query, e.g. "php_steamrip_scraper gta""#);
         std::process::exit(0);
     }
 
     let query = &query[1];
-    let url = format!("https://1337x.to/category-search/{}/Games/1/", query);
+    let url = format!("https://steamrip.com/?s={}", query);
 
-    let body = reqwest::blocking::get(url).expect("GET Request failed.").text().unwrap();
+    let body = reqwest::blocking::get(url)
+        .expect("GET Request failed.")
+        .text()
+        .expect("Couldn't format body as text.");
     let document = Html::parse_document(&body);
-    let selector = Selector::parse(r#"tbody > tr > td > a"#).unwrap();
+    let selector = Selector::parse(r#"div > div > h2 > a"#).expect("Couldn't parse title.");
 
     let mut results: Vec<String> = vec![];
 
     for title in document.select(&selector) {
-        if title.html().contains("/torrent/") && results.len() < 6 {
-            results.push(format!("https://1337x.to{}", title.value().attr("href").unwrap().to_string()));
+        if title.html().contains("-free-download/") && results.len() < 6 {
+            results.push(format!(
+                "https://steamrip.com/{}",
+                title
+                    .value()
+                    .attr("href")
+                    .expect("Couldn't fetch game page.")
+            ));
         }
     }
 
@@ -34,68 +57,81 @@ fn main() {
 }
 
 fn scan_page(url: String) {
-
-    let body = reqwest::blocking::get(url).expect("GET Request failed.").text().unwrap();
+    let body = reqwest::blocking::get(url)
+        .expect("GET Request failed.")
+        .text()
+        .expect("Couldn't format body as text.");
     let document = Html::parse_document(&body);
-    let title_selector = Selector::parse(r#"h1"#).unwrap();
-    let size_selector = Selector::parse(r#"ul > li > span"#).unwrap();
-    let magnet_selector = Selector::parse(r#"ul > li > a"#).unwrap();
+    let title_selector = Selector::parse(r#"header > div > h1"#).expect("Couldn't parse title.");
+    let size_selector = Selector::parse(r#"div > ul > li"#).expect("Couldn't parse filesize.");
+    let download_selector = Selector::parse(r#"p > a"#).expect("Couldn't parse download.");
 
     let mut titles: Vec<String> = vec![];
     let mut sizes: Vec<String> = vec![];
-    let mut magnets: Vec<String> = vec![];
+    let mut downloads: Vec<String> = vec![];
 
     for title in document.select(&title_selector) {
         titles.push(title.inner_html());
     }
 
     for size in document.select(&size_selector) {
-        if size.inner_html().contains("GB") 
-            || size.inner_html().contains("MB") 
+        if size.inner_html().contains("GB")
+            || size.inner_html().contains("MB")
             || size.inner_html().contains("KB")
         {
-            let html = size.inner_html();
-            let split: Vec<&str> = html.split("<").into_iter().collect();
-            let size = split[0];
-            sizes.push(size.to_string());
+            sizes.push("Size not available for this distributor".to_string());
         }
     }
 
-    for magnet in document.select(&magnet_selector) {
-        if magnet.inner_html().contains("Magnet Download") {
-            magnets.push(magnet.value().attr("href").unwrap().to_string())
+    for download in document.select(&download_selector) {
+        if download.inner_html().contains("DOWNLOAD HERE") {
+            downloads.push(
+                download
+                    .value()
+                    .attr("href")
+                    .expect("Couldn't append download to vector.")
+                    .to_string(),
+            )
         }
     }
 
     let title = &titles[0];
     let size = &sizes[0];
-    let magnet = &magnets[0];
+    let download = &downloads[0];
 
-    write_to_json(title.to_string(), size.to_string(), magnet.to_string());
-
+    write_to_json(title.to_string(), size.to_string(), download.to_string());
 }
 
-fn write_to_json(title: String, size: String, magnet: String) {
-
+fn write_to_json(title: String, size: String, download: String) {
+    println!("Caching:  {}", title);
     let jsoncontent = format!(
-        r#"{{ "title": "{}", "size": {}, "magnet": "{}" }}
+        r#"{{ "title": " {} ", "size": "{}", "download": "https:{}" }}
 "#,
-        title, size, magnet
+        title, size, download
     );
 
-    let dir_string = format!(r"C:\Users\{}\AppData\Roaming\Project Black Pearl\", whoami::username());
+    let dir_string = format!(
+        r"C:\Users\{}\AppData\Roaming\Project Black Pearl\",
+        whoami::username()
+    );
     let dir_path = Path::new(&dir_string);
-    let file_string = format!(r"{}\Cache.json", dir_path.display().to_string());
-    let filepath = Path::new(&file_string);
+    let file_string = format!(r"{}\STEAMRIP_Cache.json", dir_path.display());
+    let file_path = Path::new(&file_string);
 
     if !dir_path.exists() {
-        fs::create_dir_all(dir_path).unwrap();
-    }
-    
-    if !filepath.exists() {
-        File::create(filepath).unwrap();
+        fs::create_dir_all(dir_path).expect("Couldn't create cache directory.");
     }
 
-    let mut file = fs::OpenOptions::new().write(true).append(true).open(filepath).unwrap();
-    file.write_all(jsoncontent.as_bytes()).unwrap();
+    if !file_path.exists() {
+        File::create(file_path).expect("Couldn't create cache file.");
+    }
+
+    let mut file = fs::OpenOptions::new()
+        .write(true)
+        .append(true)
+        .open(file_path)
+        .expect("Couldn't open cache file.");
+
+    file.write_all(jsoncontent.as_bytes())
+        .expect("Couldn't write bytes to file.");
 }
